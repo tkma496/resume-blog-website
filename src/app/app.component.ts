9088ae4534e5a17af5baa1d35eadd649f5b9581a
Nolan Hansen <11377973+tkma496@users.noreply.github.com>
2025-04-09 12:16:32 -0500

    Made GSAP and ScrollTrigger Global and working on more animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component, } from "@angular/core";
-
+import { Component, OnInit, } from "@angular/core";
+import { Router, NavigationEnd, } from '@angular/router';
+import gsap from 'gsap';
+import { ScrollTrigger, } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: "app-root",
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{
+export class AppComponent implements OnInit {
+
   title = 'Fixer Website';
+
+  constructor(private router: Router) {}
+ngOnInit(): void {
+    gsap.registerPlugin(ScrollTrigger);
+
+    // 👇 Set global config BEFORE using ScrollTriggers
+    ScrollTrigger.config({
+      ignoreMobileResize: true,
+      autoRefreshEvents: 'DOMContentLoaded,load'
+    });
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Allow the DOM to finish updating first
+        setTimeout(() => ScrollTrigger.refresh(), 100);
+      }
+    });
+  }
 }
 
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,11 +1,4 @@
 import { AfterViewInit, Component, ElementRef, HostBinding, ViewChild } from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition
-} from '@angular/animations';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
@@ -13,57 +6,36 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
-  /*This code is used along with Material Animations to provide the slide-in
-  effect for a heading. However, unline when using GSAP and ScrollTrigger, it just slides in the second it crosses the line on the screen and can not be easily reversed. To control it with a scroll like Parallax that is where you need and Angular approved plug-in like GSAP.
-  animations: [
-    trigger('slideIn', [
-      state('hidden', style({
-        opacity: 0,
-        transform: 'translateX(-10rem)'
-      })),
-      state('visible', style({
-        opacity: 1,
-        transform: 'translateX(0)'
-      })),
-      transition('hidden => visible', [
-        animate('600ms ease-out')
-      ])
-    ])
-  ],*/
 })
 
 export class HomePageComponent implements AfterViewInit {
   @ViewChild('servicesCardTitle', { static: false }) servicesCardTitle!: ElementRef;
-  animationState = 'hidden';
+  //animationState = 'hidden';
   //Image assets to later be loaded in a CMS
   aboutMeImageUrl="../../assets/images/POWThin.jpg";
   homepageTitleImage="../../assets/images/Homepage HD belt.jpg";
 
-    ngAfterViewInit(): void {
-      gsap.registerPlugin(ScrollTrigger);
-      // 👇 Set global config BEFORE using ScrollTriggers
-    ScrollTrigger.config({
-      ignoreMobileResize: true,
-      autoRefreshEvents: 'DOMContentLoaded,load'
-    });
+  ngAfterViewInit(): void {
 
-//   gsap.utils.toArray<HTMLElement>('this.servicesCardTitle.nativeElement').forEach((el) => {
-  gsap.utils.toArray<HTMLElement>('.servicesCardTitle').forEach((el) => {
-      gsap.from(el, {
-        x: -300,
-        opacity: 0,
-        duration: 5,
-        ease: "elastic.out(1.5,0.2)",
-        scrollTrigger: {
-          //trigger: this.servicesCardTitle.nativeElement,
-          trigger: el,
-          start: 'top 80%',
-          end: 'top 30%',
-          toggleActions: "restart none restart none", // no scrub!
-          //scrub: true, // 👈 animation syncs with scroll
-          markers: true // 👈 shows where trigger happens (for dev)
-        }
+
+    requestAnimationFrame(() => {
+      gsap.utils.toArray<HTMLElement>('.servicesCardTitle').forEach((el) => {
+        gsap.from(el, {
+          x: -300,
+          opacity: 0,
+          duration: 5,
+          ease: "elastic.out(1.5,0.2)",
+          scrollTrigger: {
+            //trigger: this.servicesCardTitle.nativeElement,
+            trigger: el,
+            start: 'top 80%',
+            end: 'top 30%',
+            toggleActions: "restart none restart none", // no scrub!
+            //scrub: true, // 👈 animation syncs with scroll
+            markers: true // 👈 shows where trigger happens (for dev)
+          }
+        })
       })
-    });      
+    });
   }
 }
